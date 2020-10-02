ff52502ac9e258cae1d3b534d65efb3d3b352010
OprCalf <393526884@qq.com>
2020-10-02 11:02:46 +0800

    基于mybatisplus的二次封装

---
+++ modules-boots-mybatisplus/src/main/java/com/modules/boots/mybatisplus/App.java
@@ -0,0 +1,13 @@
+package com.modules.boots.mybatisplus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
