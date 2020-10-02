82db6eade01959bae2e5c50abadae86bd95f1a37
OprCalf <393526884@qq.com>
2020-10-02 13:04:07 +0800

    新增Mybatis-plus项目包

---
+++ modules-boots-mp/src/main/java/com/modules/boots/mp/App.java
@@ -0,0 +1,13 @@
+package com.modules.boots.mp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
