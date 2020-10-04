ad911f78fa60066f7ad5c74a974e43ac5dc694e1
OprCalf <393526884@qq.com>
2020-10-04 16:43:18 +0800

    新增redis缓存器

---
+++ modules-boots/modules-boots-cache-redis/src/main/java/com/modules/boots/cache/redis/MybatisRedisCache.java
@@ -0,0 +1,98 @@
+/**
+ * All rights Reserved, Designed By 林溪
+ * Copyright:    Copyright(C) 2016-2030
+ */
+
+package com.modules.boots.cache.redis;
+
+import java.util.Set;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import org.apache.ibatis.cache.Cache;
+import org.springframework.data.redis.connection.RedisServerCommands;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.CollectionUtils;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * redis缓存器
+ * @author：林溪
+ * @date：2020年10月4日
+ */
+@Slf4j
+public class MybatisRedisCache implements Cache {
+
+    // 读写锁
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock(true);
+
+    // 这里使用了redis缓存，使用springboot自动注入
+    private RedisTemplate<String, Object> redisTemplate;
+
+    private String id;
+
+    public MybatisRedisCache(final String id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Cache instances require an ID");
+        }
+        this.id = id;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    @SneakyThrows(Exception.class)
+    public void putObject(Object key, Object value) {
+        if (value != null) {
+            redisTemplate.opsForValue().set(key.toString(), value);
+        }
+    }
+
+    @Override
+    @SneakyThrows(Exception.class)
+    public Object getObject(Object key) {
+        if (key != null) {
+            return redisTemplate.opsForValue().get(key.toString());
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    @SneakyThrows(Exception.class)
+    public Object removeObject(Object key) {
+        if (key != null) {
+            redisTemplate.delete(key.toString());
+        }
+        return null;
+    }
+
+    @Override
+    @SneakyThrows(Exception.class)
+    public void clear() {
+        log.debug("清空缓存");
+        final Set<String> keys = redisTemplate.keys("*:" + id + "*");
+        if (!CollectionUtils.isEmpty(keys)) {
+            redisTemplate.delete(keys);
+        }
+    }
+
+    @Override
+    @SneakyThrows(Exception.class)
+    public int getSize() {
+        final Long size = redisTemplate.execute((RedisCallback<Long>) RedisServerCommands::dbSize);
+        return size.intValue();
+    }
+
+    @Override
+    @SneakyThrows(Exception.class)
+    public ReadWriteLock getReadWriteLock() {
+        return readWriteLock;
+    }
+}
