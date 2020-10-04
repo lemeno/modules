a4249adc33fb329cd90c476b9d3ed4c44bb518a9
OprCalf <393526884@qq.com>
2020-10-04 08:03:23 +0800

    新增DB项目

---
+++ modules-boots/modules-boots-db/src/main/java/com/modules/boots/db/filling/MysqlMetaHandler.java
@@ -0,0 +1,18 @@
+/**
+ * All rights Reserved, Designed By 林溪
+ * Copyright:    Copyright(C) 2016-2030
+ */
+
+package com.modules.boots.db.filling;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+
+/**
+ * MYSQL字段填充处理器
+ * @author：林溪
+ * @date：2020年10月3日
+ */
+
+public interface MysqlMetaHandler extends MetaObjectHandler {
+
+}
---
+++ modules-boots/modules-boots-db/src/main/java/com/modules/boots/db/filling/OracleMetaHandler.java
@@ -0,0 +1,18 @@
+/**
+ * All rights Reserved, Designed By 林溪
+ * Copyright:    Copyright(C) 2016-2030
+ */
+
+package com.modules.boots.db.filling;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+
+/**
+ * ORACLE字段填充处理器
+ * @author：林溪
+ * @date：2020年10月3日
+ */
+
+public interface OracleMetaHandler extends MetaObjectHandler {
+
+}
